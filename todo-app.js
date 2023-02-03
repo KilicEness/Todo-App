ee05e06b8574d00acc59ec81dafe463f4efe079e
enes <eneskiliicc06@gmail.com>
2023-02-03 17:00:33 +0300

    filtering added and restored

--- todo-app.js
+++ todo-app.js
@@ -15,21 +15,48 @@ const todos = [{
     completed: true
 }]
 
-const incompleteTodos = todos.filter(function(todo) {
-    return !todo.completed
-})
+//filter search, searching words
+const filters = {
+    searchText: ''
+}
 
-const summary = document.createElement('h2')
-summary.textContent = `you have ${incompleteTodos.length} todos left`
-document.querySelector('body').appendChild(summary)
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function () {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
 
-todos.forEach(function(todo) {
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    document.querySelector('body').appendChild(p)
-})
+    //show incompleted todos
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `you have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    //show todos
+    filteredTodos.forEach(function (todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+
+renderTodos(todos, filters)
 
 //Listen for todo creation
-document.querySelector('#add-todo').addEventListener('click', function(e){
+document.querySelector('#add-todo').addEventListener('click', function (e) {
     e.target.textContent = 'Clicked for add'
+})
+
+//Listen for todo text change
+document.querySelector('#new-todo').addEventListener('input', function (e) {
+    console.log(e.target.value)
+})
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
 })
