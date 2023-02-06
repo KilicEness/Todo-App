e8b7bf7178b2957660dc04680cacf4b58ef95de9
enes <eneskiliicc06@gmail.com>
2023-02-06 23:58:59 +0300

    delete dummy data and local storage JSON operations

--- todo-app.js
+++ todo-app.js
@@ -1,24 +1,15 @@
-const todos = [{
-    text: 'Order cat food',
-    completed: false
-}, {
-    text: 'Clean the kitchen',
-    completed: true
-}, {
-    text: 'Buy food',
-    completed: true
-}, {
-    text: 'Do work',
-    completed: false
-}, {
-    text: 'Exercise',
-    completed: true
-}]
+let todos = []
 
 //filter search, searching words
 const filters = {
     searchText: '',
     hideCompleted: false
+}
+
+const todosJSON = localStorage.getItem('todos')
+
+if(todosJSON !== null) {
+    todos = JSON.parse(todosJSON)
 }
 
 const renderTodos = function (todos, filters) {
@@ -61,6 +52,7 @@ document.querySelector('#new-todo').addEventListener('submit', function (e) {
         text: e.target.elements.text.value,
         completed: false
     })
+    localStorage.setItem('todos', JSON.stringify(todos))
     renderTodos(todos, filters) // virgül yerine nokta kullanmışım uğraştırdı xd
     e.target.elements.text.value = ''
 })
